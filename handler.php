0771796a8e90250827d59d8c9e6c051d9d0053cf
Maddy09123 <144242311+Maddy09123@users.noreply.github.com>
2023-10-25 09:28:55 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,23 +3,23 @@
   <head>
   <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <title>Welcome Page</title>
   </head>
 
   <body>
-    
+
     <?php
     $userName = "";
     if ( isset( $_POST['radius'] ) ){
       $radius = $_POST['radius'];
     }
     echo "<h1>For a Circle of Radius ".$radius."</h1>\n";
-    echo "<p>Area = \n";
-    echo "<p>Circumference = \n";
+    echo "<p>Area = ".(pi()*$radius**2)."</p>\n";
+    echo "<p>Circumference = ".(2*pi()*$radius)."</p>\n";
     ?>
-    
+
   </body>
-  
+
 </html>
